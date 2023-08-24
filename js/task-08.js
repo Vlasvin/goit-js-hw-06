6f6b1c2aeb24d784fdd5cabe05511f7514ef6200
Aleksey <103183090+Vlasvin@users.noreply.github.com>
2023-08-24 14:50:13 +0300

    task_8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const elements = event.currentTarget.elements;
+  const formData = new FormData(event.currentTarget);
+  const userData = {};
+  formData.forEach((value, name) => {
+    userData[name] = value;
+  });
+  if ((elements.email.value && elements.password.value) === '') {
+    window.alert('Всі поля повинні бути заповнені');
+    return;
+  }
+  console.log(userData);
+  form.reset();
+}
