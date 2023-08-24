918dc0f0f58d591e7dd19b9a07fbdfa4f09b2b1b
Aleksey <103183090+Vlasvin@users.noreply.github.com>
2023-08-24 15:30:50 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const colorName = document.querySelector('.color');
+const currentColor = document.querySelector('body');
+const button = document.querySelector('button');
+button.addEventListener('click', changeBackColor);
+function changeBackColor() {
+  currentColor.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = currentColor.style.backgroundColor;
+}
