0787d59156c44b956b713e4ce4eff34c9c3d2540
Aleksey <103183090+Vlasvin@users.noreply.github.com>
2023-08-29 12:55:53 +0300

    fix_task_01

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
 const categories = document.querySelectorAll('li.item');
 console.log(`Number of categories:`, categories.length);
 categories.forEach(element => {
-  const category = element.querySelector('h2');
-  const elements = element.querySelectorAll('li');
+  const category = element.firstElementChild;
+  const elements = element.children[1].children.length;
   console.log(`Category:`, category.textContent);
-  console.log(`Elements:`, elements.length);
+  console.log(`Elements:`, elements);
 });
